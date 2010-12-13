1954ddc8157666a393872edbd7f48eed7326a5aa
Stefan Simroth <stefan.simroth@gmail.com>
2010-12-13 21:57:19 +0100

    working plugin v1.0 ready to use.

--- plugin.php
+++ plugin.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Pseudonymize Plugin
 Plugin URI: http://github.com/ubicoo/yourls-pseudonymize
-Description: Pseudonymize IP addresses.
-Version: 0.1
+Description: Pseudonymize IP addresses (remove last 2 segments/bytes).
+Version: 1.0
 Author: Ubicoo
 Author URI: http://www.ubicoo.com
 */
@@ -12,7 +12,10 @@ Author URI: http://www.ubicoo.com
 yourls_add_filter( 'get_IP', 'ubicoo_pseudonymize_IP' );
  
 function ubicoo_pseudonymize_IP( $value ) {
-	//TODO change last segment to 0 (if not already)[can this happen?]
-	return $value;
+	$segments = explode(".", $value);
+	$segments[2] = 0;
+	$segments[3] = 0;
+	$pseudo_IP = implode(".", $segments);
+	return $pseudo_IP;
 }
 
